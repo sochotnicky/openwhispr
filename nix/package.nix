19f6968b122994235c5ddd775af174e76533fd03
Stanislav Ochotnický <stanislav@ochotnicky.com>
2026-07-13 16:11:47 +0200

    nix: fork release v1.7.5-sochotnicky

--- nix/package.nix
+++ nix/package.nix
@@ -10,7 +10,7 @@ let
 
   src = fetchurl {
     url = "https://github.com/sochotnicky/openwhispr/releases/download/v${version}/OpenWhispr-${version}-linux-x86_64.AppImage";
-    hash = "sha256-ZUfAJUZ0CVB66Sa9OMRUjSwwSiJ21y7UksmaNjDd3bg=";
+    hash = "sha256-t7OSS19qxheH5KAxOygrxpYtl0VTVY038HqcRiLtObI=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
